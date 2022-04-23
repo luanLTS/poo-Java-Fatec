caa626a4f63c643023d2c6b6a67b6ced581ba6e4
Luan Teixeira <luanlts@outlook.com>
2022-04-23 10:16:23 -0300

    modificando a Connection

--- BD_pesooa/src/ConnectionFactory.java
+++ BD_pesooa/src/ConnectionFactory.java
@@ -1,10 +1,12 @@
 package src;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
 
-    // geralmente as informacoes de usuario e senha ficam em um arquivo a parte que nao faz parte do versionamento
+    // geralmente as informacoes de usuario e senha ficam em um arquivo a parte que
+    // nao faz parte do versionamento
     // java -> .properts
     // node / JS -> .env
     private String user = "root";
@@ -12,29 +14,27 @@ public class ConnectionFactory {
     private String host = "localhost";
     private String port = "3306";
     private String db = "pessoas";
-   
+
     public Connection obterConexao() {
-       String connection = String.format(
-            "jdbc:mysql://%s:%s/%s",
-            host,
-            port,
-            db
-       );
+        String connection = String.format(
+                "jdbc:mysql://%s:%s/%s",
+                host,
+                port,
+                db);
         try {
             return DriverManager.getConnection(
-               connection,
-               user,
-               passwd
-            );
+                    connection,
+                    user,
+                    passwd);
         } catch (Exception e) {
             e.printStackTrace(); // pilha de chamada de metodo
-            // System.out.println("Não conectou erro -> " + e);
+            System.out.println("Não conectou erro -> " + e);
             return null;
         }
-   }
+    }
 
-//    public static void main(String[] args) {
-//        ConnectionFactory factory = new ConnectionFactory();
-//        factory.obterConexao();
-//    }
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        System.out.println(factory.obterConexao());
+    }
 }
